4a8c2b96677a048e561e6e57ea3956065f9f5b95
pirat <azerty>
2017-06-27 19:20:19 +0200

    Angular get started

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'my-angular-app'), // imports firebase/app needed for everything
+    AngularFireDatabaseModule // imports firebase/database, only needed for database features
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ my-app/src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBdlwvQlLi5SL5k0x4ovlS_wzFgXRcLF7M",
+    authDomain: "angularfire-d883f.firebaseapp.com",
+    databaseURL: "https://angularfire-d883f.firebaseio.com",
+    projectId: "angularfire-d883f",
+    storageBucket: "",
+    messagingSenderId: "152372715416"
+  }
+};
